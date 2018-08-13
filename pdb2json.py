a9a684c74176639e3680b3421a39c41449b938fe
Sam Ireland <sam.ireland.uk@gmail.com>
2018-08-13 19:39:22 +0100

    Make dictionaries traversable using URLs

--- pdb2json.py
+++ pdb2json.py
@@ -1,26 +1,25 @@
 import os
 import binascii
 import atomium
-from django.urls import path
+from django.urls import re_path
 from django.http import JsonResponse
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 SECRET_KEY = binascii.hexlify(os.urandom(24)).decode()
-
 VERSION = "0.1.0"
-
 ALLOWED_HOSTS = []
-
 DEBUG = True
-
 ROOT_URLCONF = __name__
 
 def pdb(request, id):
-    d = atomium.fetch(id, file_dict="file" in request.GET, data_dict=True)
-    return JsonResponse(d, json_dumps_params={"indent": 4})
+    keys = [k for k in request.path.split("/") if k]
+    d = atomium.fetch(
+     keys.pop(0), file_dict="file" in request.GET, data_dict=True
+    )
+    while keys:
+        d = d[keys.pop(0)] if isinstance(d, dict) else d[int(keys.pop(0))]
+    return JsonResponse(d, safe=False, json_dumps_params={"indent": 4})
 
 urlpatterns = [
- path(r"<id>/", pdb),
- path(r"<id>", pdb),
+ re_path(r"^(?P<id>.*)/?", pdb),
 ]
