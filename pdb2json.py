a980d460b0d088a0d1601ed6e92129b670531c43
Sam Ireland <sam.ireland.uk@gmail.com>
2018-08-13 19:56:41 +0100

    Add root URL

--- pdb2json.py
+++ pdb2json.py
@@ -1,7 +1,7 @@
 import os
 import binascii
 import atomium
-from django.urls import re_path
+from django.urls import re_path, path
 from django.http import JsonResponse
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,6 +20,20 @@ def pdb(request, id):
         d = d[keys.pop(0)] if isinstance(d, dict) else d[int(keys.pop(0))]
     return JsonResponse(d, safe=False, json_dumps_params={"indent": 4})
 
+def root(request):
+    return JsonResponse({
+     "data_dict": request.build_absolute_uri() + "1LOL/",
+     "experiment_data": request.build_absolute_uri() + "1LOL/experiment/",
+     "technique": request.build_absolute_uri() + "1LOL/experiment/technique/",
+     "data_dict_from_cif": request.build_absolute_uri() + "1LOL.cif/",
+     "pdb_data_dict": request.build_absolute_uri() + "1LOL?file",
+     "cif_data_dict": request.build_absolute_uri() + "1LOL.cif?file",
+     "source": "https://github.com/samirelanduk/pdb2json/",
+     "author": "https://samireland.com/",
+     "twitter": "https://twitter.com/samirelanduk/"
+    }, json_dumps_params={"indent": 4})
+
 urlpatterns = [
+ path("", root),
  re_path(r"^(?P<id>.*)/?", pdb),
 ]
